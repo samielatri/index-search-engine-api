b7772985f102cc7212aae42e9b839ddfc23d33e7
samielatri <samielatri.pro@gmail.com>
2021-02-10 00:28:28 +0100

    <Adding> Field interface implemented

--- src/main/java/service/business/Cluster.java
+++ src/main/java/service/business/Cluster.java
@@ -16,16 +16,26 @@ import java.util.List;
  */
 public class Cluster { // Table
     // TODO: implement Map logic
+    // TODO: Cluster becomes a Page and Cluster class is the collection of pages /!\
     List<Document> documentList; // list of documents
     List<List<Pair<Integer, Integer>>> map; // content of cluster
     /*
-    // representation of the map :
+    // representation of the map : (inverted search index)
                         -------------------------------------
     key[0] ->  (num_doc):(occ)  |(num_doc):(occ)  |
     key[1] ->  (num_doc):(occ)  |(num_doc):(occ)  |
                         --------------------------------------
+                        + evalutation function
     What stands for what ? :
        num_doc : number of document
        occ : occurrence of the documentList[x] in the document of num_doc
   */
+
+ // WARNING : this logic is implemented only in case no fields /!\
+
+ // OTHER LOGIC :
+    // Use Header Class
+    // and normal two dimensional arrays
+
+ // After V0 : see binary trees
 }
---
+++ src/main/java/service/business/Field.java
@@ -0,0 +1,26 @@
+package service.business;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.Serializable;
+
+/**
+ * Created bu PacLab
+ * User: sami
+ * Date: 2/10/2021
+ * Time: 11:38 AM
+ * Package: service.business
+ */
+public interface Field extends Serializable {
+
+    // TODO: change String operator to Enum operator
+    public int compare(Field field, String operator);
+
+    // TODO: change to Type
+    public void getType();
+
+    void serialize(DataOutputStream dataOutputStream) throws IOException;
+    public int hashcode();
+    public boolean equals(Object filedObject);
+    public String toString();
+}
