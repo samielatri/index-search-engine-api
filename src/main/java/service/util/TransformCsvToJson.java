70f61df3850ef2867c424d0b8ee18f0b7e39cd18
samielatri <samielatri.pro@gmail.com>
2021-02-09 15:10:52 +0100

    <Adding> service.util : add service util class - TransformCsvToJson.

---
+++ src/main/java/service/util/TransformCsvToJson.java
@@ -0,0 +1,39 @@
+package service.util;
+
+/**
+ * Created bu PacLab
+ * User: sami
+ * Date: 2/9/2021
+ * Time: 3:02 PM
+ * Package: service.util
+ *
+ * Class name: TransformCsvToJson
+ * Class description: A TransformCsvToJson is a tool to transform Csv to Json.
+ */
+public class TransformCsvToJson {
+    /**
+     * main : main entry of TransformCsvToJson class to test member methods of the class.
+     * @param args : an array of String meant to hold passed command line arguments.
+     */
+    public static void main(String [] args) {
+        // readCsvUsingScanner("test.csv");
+        // readCsvUsingBufferReader("test.csv");
+    }
+
+    /**
+     * readCsvUsingScanner : static method to read csv file using Scanner.
+     * @param filePath : String that represents file path of the csv file.
+     */
+    private static void readCsvUsingScanner(String filePath) {
+        // TODO : implement logic
+    }
+
+    /**
+     * readCsvUsingBufferReader : static method to read csv file using BufferReader.
+     * @param filePath : A String that represents file path of the csv file.
+     */
+    private static void readCsvUsingBufferReader(String filePath) {
+        // TODO : implement logic
+    }
+}
+
