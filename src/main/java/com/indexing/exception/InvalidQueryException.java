18c54a5fcbd1b06d5a88b6c0b535db3215bb5081
samielatri <samielatri.pro@gmail.com>
2021-05-14 01:35:07 +0200

    <Documentation> Add doccomments on InvalidQueryException

--- src/main/java/com/indexing/exception/InvalidQueryException.java
+++ src/main/java/com/indexing/exception/InvalidQueryException.java
@@ -1,12 +1,33 @@
+/* package */
 package com.indexing.exception;
+
+/* On-Demand Imports */
+
+/* Specific Imports */
 
 /**
  * Created bu PacLab
  * User: PacLab
+ * Date: 2021
+ *
+ * Class that handles query exceptions,
+ * extends Exception.
  */
 
-public class InvalidQueryException extends Exception {
+public class InvalidQueryException
+        extends Exception {
+    /* Data members */
+
+    /* Constructors */
+
+    /**
+     *
+     * @param message String represents message of the exception
+     */
     public InvalidQueryException(String message) {
         super(message);
     }
+
+    /* Methods */
+
 }
